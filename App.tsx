b536fd026ff14500191d85cd850773fb530b0cd9
NhuongDT <inftech.ts91@gmail.com>
2024-05-08 09:39:46 +0700

    version 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import dashboard from "./page/dashboard/dashboard";
+import PageInvoice from "./page/invoice/PageInvoice";
+import Login from "./page/login/login";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <Stack.Screen name="login" component={Login} /> */}
+      <Stack.Navigator>
+        <Stack.Screen name="Đăng nhập" component={Login} />
+        {/* <Stack.Screen name="home" component={dashboard} />
+        <Stack.Screen name="invoice" component={PageInvoice} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
