fb9a0b54505b62983359f56a91af8911b7ffa883
NhuongDT <inftech.ts91@gmail.com>
2025-03-20 16:08:44 +0700

    prettierrc: add comment for rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'none',
-  endOfLine: 'auto',
-  printWidth: 120,
-  tabWidth: 4
+    arrowParens: 'avoid', // Bỏ dấu () nếu arrow function có 1 tham số
+    bracketSameLine: false, // JSX: dấu > xuống dòng
+    bracketSpacing: true, // Thêm dấu cách giữa dấu ngoặc {} trong object.
+    singleQuote: true, // Dùng dấu nháy đơn ' thay vì dấu nháy đôi " cho chuỗi.
+    trailingComma: 'none', // Không thêm dấu phẩy , cuối cùng trong danh sách phần tử hoặc object.
+    endOfLine: 'auto', // Tự động chọn kiểu xuống dòng (\n, \r\n) phù hợp với hệ điều hành.
+    printWidth: 120, // Giới hạn độ dài tối đa của một dòng code là 120 ký tự.
+    tabWidth: 4 // Mỗi tab sẽ tương đương với 4 khoảng trắng
 };
